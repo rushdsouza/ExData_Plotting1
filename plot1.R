0f2092570b34318509bdd1f7dd9541d065161f23
rushdsouza <rushdsouza@gmail.com>
2015-04-11 00:37:09 +0530

    plo1.R changes
    removed merger of date and time columns.

--- plot1.R
+++ plot1.R
@@ -7,9 +7,6 @@ power$Date <- as.Date(power$Date, format="%d/%m/%Y",tz="")
 # Filter for dates Feb 01 and 02, 2007
 power1 <- power[((power$Date == "2007-02-01") | (power$Date == "2007-02-02")), ]
 
-# Combine Date and Time columns 
-power2 <- cbind(DateTime = paste(as.character(power1$Date), power1$Time, sep=" "),power1) 
-
 hist(power1$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main = "Global Active Power")
 dev.copy(png, file = "plot1.png", width=480, height=480) ## Copy my plot to a PNG file
 dev.off() ## Don't forget to close the PNG device!
