7d0cbd2b9543018b235e723c5118e8c056673100
rushdsouza <rushdsouza@gmail.com>
2015-04-12 17:31:01 +0530

    Fixed x-axis labels in plot2
    Fixed x-axis labels in plot2

--- plot2.R
+++ plot2.R
@@ -5,17 +5,18 @@ power <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.
 power$Date <- as.Date(power$Date, format="%d/%m/%Y",tz="")
 
 # Filter for dates Feb 01 and 02, 2007
-power1 <- power[((power$Date == "2007-02-01") | (power$Date == "2007-02-02")), ]
+power1 <- power[((power$Date == "2007-02-01") | (power$Date == "2007-02-02")) |((power$Date == "2007-02-03") & power$Time == "00:00:00"), ]
 
 # Merge the columns, Date and Time into column, DateTime
-power2 <- cbind(DateTime = paste(as.character(power1$Date), power1$Time, sep=" "), Day = weekdays(as.Date(power1$Date),abbreviate=TRUE), power1) 
+power2 <- cbind(DateTime = strptime(paste(as.character(power1$Date), power1$Time, sep=" "), "%Y-%m-%d %H:%M:%S"), power1) 
 
-# Plot plot2 using qplot
-# with(power2, plot(as.numeric(DateTime), Global_active_power, xlab=unique(weekdays(as.Date(power2$DateTime),abbreviate=TRUE)), ylab="Global Active Power (kilowatts)", type="l",xaxt="n"))
-with(power2, plot(as.numeric(DateTime), Global_active_power, xlab=unique(weekdays(as.Date(power2$DateTime),abbreviate=TRUE)), ylab="Global Active Power (kilowatts)", type="l",xaxt="n"))
+# Plot plot2 using plot
+with(power2,{
+    plot(as.numeric(power2$DateTime),power2$Global_active_power, xlab="", ylab="Global Active Power (kilowatts)", type="l",xaxt="n")
+    xday <- as.POSIXct(range(power2$DateTime))
+    axis.POSIXct(1,xday, at=seq(xday[1], xday[2], by = "days"), format="%a", labels = TRUE)
+})
 
-# library(ggplot2)
-# qplot(as.numeric(DateTime), Global_active_power, data=power2,xlab=power2$Day, ylab="Global Active Power (kilowatts)", geom="path")
-
-dev.copy(png, file = "plot2.png", width=480, height=480) ## Copy my plot to a PNG file
+# Copy my plot to a PNG file, plot2.png in working directory
+dev.copy(png, file = "plot2.png", width=480, height=480)
 dev.off() ## Don't forget to close the PNG device!
